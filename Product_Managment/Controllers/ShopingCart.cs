9bc2194a53484302bc0421d5fc668cf0ebfbb8ce
fisnikHaziri <fisnik.haziri.h@gmail.com>
2024-08-17 13:12:46 +0200

    started on the ShopingCart feature

---
+++ Product_Managment/Controllers/ShopingCart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Product_Managment.Controllers
+{
+	public class ShopingCart : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Product_Managment_Model/ShopingCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Managment_Model
+{
+	public class ShopingCart
+	{
+		public int Id { get; set; }
+		public decimal TotalPrice { get; set; }
+
+		public string UserId {  get; set; }
+		public virtual ApplicationUser User { get; set; }
+		public List<Product> products {  get; set; }
+	}
+}
